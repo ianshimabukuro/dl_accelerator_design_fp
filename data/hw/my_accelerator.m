39d0a19366775f20d5f5424b833b53fcd20c747d
ian <akio_ian@hotmail.com>
2024-12-09 20:32:40 -0800

    Cleaned and formatted the project

---
+++ data/hw/my_accelerator.m
@@ -0,0 +1,8 @@
+
+num_pes: 256 
+noc_bw_cstr: 1000. 
+
+// Dont change
+l1_size_cstr: 100 
+l2_size_cstr: 3000 
+offchip_bw_cstr: 50 
---
+++ run_UNET.sh
@@ -0,0 +1,5 @@
+./maestro --HW_file='data/hw/accelerator_1.m' \
+          --Mapping_file='data/mapping/UNet_dataflow.m' \
+          --print_res=true \
+          --print_res_csv_file=true \
+          --print_log_file=false \
